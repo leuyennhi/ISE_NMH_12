daac3f377d961d9a018de46eb7c45388e46feb56
leuyennhi <43956696+leuyennhi@users.noreply.github.com>
2018-12-02 22:53:40 +0700

    Update source

---
+++ Source/HotelApp/HotelApp/BaoCaoMatDo.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HotelApp
+{
+    /// <summary>
+    /// Interaction logic for BaoCaoTyLe.xaml
+    /// </summary>
+    public partial class BaoCaoMatDo : Window
+    {
+        public BaoCaoMatDo()
+        {
+            InitializeComponent();
+        }
+    }
+}
