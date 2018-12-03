9ebb141009cd943e80c36f2d664a1f0e3c63462c
leuyennhi <43956696+leuyennhi@users.noreply.github.com>
2018-12-03 22:15:03 +0700

    update source

--- Source/HotelApp/HotelApp/LoginScreen.xaml.cs
+++ Source/HotelApp/HotelApp/LoginScreen.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace WpfSign_in
+namespace HotelApp
 {
     /// <summary>
     /// Interaction logic for LoginScreen.xaml
